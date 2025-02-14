2250de529cfe040dd5526369f3e0f462a1a8968f
prarthana1305 <prarthanarnayaka@gmail.com>
2025-02-14 14:46:57 +0530

    added main files

---
+++ MetroService/src/main/java/com/example/MetroService/MetroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.MetroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroServiceApplication.class, args);
+	}
+
+}
---
+++ MetroService/src/test/java/com/example/MetroService/MetroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MetroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
